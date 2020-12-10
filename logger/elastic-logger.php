af59982f600bd58fdcae035e7384b74eaf9d928f
Cairo Coder <cairocoder01@gmail.com>
2020-12-10 18:14:42 +0200

    map activity to ecs

--- logger/elastic-logger.php
+++ logger/elastic-logger.php
@@ -23,11 +23,100 @@ class DT_Advanced_Security_Elastic_Logger extends DT_Advanced_Security_Base_Logg
                 return;
             }
 
-            $this->post_to_api( $args );
+            $ecs = $this->map_activity_to_ecs( $args );
+
+            $this->post_to_api( $ecs );
 
         } catch ( Exception $ex ) {
             dt_write_log( json_encode( $ex ) );
         }
+    }
+
+    private function map_activity_to_ecs( $args ) {
+
+        $plugin_data = get_plugin_data( dirname( __FILE__, 2 ) . '/disciple-tools-advanced-security.php' );
+        $version = $plugin_data['Version'];
+
+        $action = $args['action'];
+        $object_type = $args['object_type'];
+        $http_regex = '/http[s]?:\/\//';
+
+        $ecs = [
+            '@timestamp' => gmdate( DateTimeInterface::RFC3339_EXTENDED, $args['hist_time'] ),
+            'labels' => $args,
+            'message' => "action:$action object_type:$object_type",
+            'event' => [
+                'action' => "$object_type:$action",
+                'original' => json_encode( $args ),
+                'module' => 'dt-advanced-security-plugin',
+                'url' => $this->get_url(),
+                'kind' => 'event',
+                // 'category' => '', // mapped below and blank if no match
+                'type' => [],
+            ],
+            'host' => [
+                'hostname' => preg_replace( $http_regex, "", home_url() ),
+            ],
+            'agent' => [
+                'name' => preg_replace( $http_regex, "", home_url() ),
+                'type' => 'dt-advanced-security-plugin',
+                'version' => $version
+            ],
+            'user' => [
+                'id' => isset( $args['user_id'] ) ? $args['user_id'] : null,
+                'roles' => isset( $args['user_caps'] ) ? [ $args['user_caps'] ] : null,
+            ]
+        ];
+
+        // authentication -> event.category: authentication
+        $auth_actions = [ 'logged_in', 'login_failed', 'password_reset_request', 'password_reset' ];
+        if ( $object_type == 'User' && in_array( $action, $auth_actions ) ) {
+            $ecs['event']['category'] = 'authentication';
+        }
+
+        // user changes -> event.category: iam
+        $iam_actions = [ 'created', 'add_role', 'remove_role', 'add_to_site', 'remove_from_site', 'granted_super_admin', 'revoked_super_admin', 'deleted' ];
+        if ( $object_type == 'User' && in_array( $action, $iam_actions ) ) {
+            $ecs['event']['category'] = 'iam';
+            $ecs['event']['type'][] = 'user';
+            if ( $action == 'created' ) {
+                $ecs['event']['type'][] = 'creation';
+            } else if ( $action == 'deleted' ) {
+                $ecs['event']['type'][] = 'deletion';
+            } else {
+                $ecs['event']['type'][] = 'change';
+            }
+        }
+
+        // plugin -> event.category: package
+        if ( $object_type == 'plugin' || $object_type == 'core' ) {
+            $ecs['event']['category'] = 'package';
+
+            if ( in_array( $action, [ 'delete', 'delete_fail', 'deactivate_network', 'deactivate' ] ) ) {
+                $ecs['event']['type'][] = 'deletion';
+                if ( $action == 'delete_fail' ) {
+                    $ecs['event']['outcome'] = 'failure';
+                }
+            } else if ( in_array( $action, [ 'activate_network', 'activate' ] ) ) {
+                $ecs['event']['type'][] = 'installation';
+            } else if ( in_array( $action, [ 'update' ] ) ) {
+                $ecs['event']['type'][] = 'change';
+            }
+        }
+
+        // export -> event.category: process
+        if ( $action == 'export' ) {
+            $ecs['event']['category'] = 'process';
+            $ecs['event']['type'][] = 'access';
+        }
+
+        // site_link_system -> event.category: database
+        if ( $object_type == 'site_link_system' ) {
+            $ecs['event']['category'] = 'database';
+            $ecs['event']['type'][] = 'change';
+        }
+
+        return $ecs;
     }
 
     /**
@@ -67,6 +156,24 @@ class DT_Advanced_Security_Elastic_Logger extends DT_Advanced_Security_Base_Logg
             // Success
             return true;
         }
+    }
+
+    /**
+     * @return string
+     */
+    private function get_url() {
+        if ( !isset( $_SERVER['REQUEST_URI'] ) ) {
+            return null;
+        }
+        $uri = sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) );
+
+        if ( !isset( $_SERVER['HTTP_HOST'] ) ) {
+            return $uri;
+        }
+        $host = sanitize_text_field( wp_unslash( $_SERVER['HTTP_HOST'] ) );
+
+        return ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? "https" : "http" ) .
+            "://$host$uri";
     }
 }
 DT_Advanced_Security_Elastic_Logger::instance();
